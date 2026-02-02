8dec1e082784830fbc089fad01e9c20590d9d4ba
krot144100-lang <krot144100@gmail.com>
2026-02-02 15:19:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # This looks for index.html inside the 'templates' folder
+def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
